1555cee2e48e856493623549ad351fea6743968b
Amaan Rizvi <amaan1712@gmail.com>
2021-04-02 20:21:10 +0100

    meow

--- src/Utils.test.js
+++
@@ -1,7 +0,0 @@
-const { shuffle } = require('./Utils');
-
-describe('Shuffle test', () => {
-    it('takes in an array and returns a shuffled version of it.', () => {
-        expect(shuffle([1,2,3,4])).not.toEqual([1,2,3,4])
-    })
-});
